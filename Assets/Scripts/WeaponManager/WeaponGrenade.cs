dfcc1422a45defa3faa507abccc4bc508807cc8f
Guillermo Felix Romero <guillermofelixromero@gmail.com>
2022-10-27 19:36:13 -0300

    un poco de orden

---
+++ Assets/Scripts/WeaponManager/WeaponGrenade.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponGrenade : WeaponBase
+{
+    public override void OnActive() { }
+    public override void OnDeactive() { }
+    protected override void OnPressDown() { }
+    protected override void OnPressUp() { }
+    protected override void OnTickPressed(float DeltaTime) { }
+}
